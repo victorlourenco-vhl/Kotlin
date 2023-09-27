b90126c0860dc16278261bdd8ddd16529bf8e4b2
Victor Lourenço <victorhlo12@hotmail.com>
2023-09-27 13:34:06 -0300

    LEARN: Entendendo os tipos de variáveis

---
+++ Kotlin/src/main/kotlin/com/curso/kotlin/_01variaveis/KotlinTypes.kt
@@ -0,0 +1,26 @@
+package com.curso.kotlin._01variaveis
+
+fun main() {
+
+    val frase = "Sou uma String"
+    val pi = 3.14
+    val idade = 15
+
+    val palavra: String
+    val decimal: Double
+    val inteiro: Int
+
+    val decimalDouble = 1.99
+    val flutuante = 1.9F
+    val inteiroLong = 5436546365L
+    val inteiroSimples = 2353
+    val short: Short = 128
+    val byte: Byte = 127
+
+    val aLong = 123_456_789
+    val mesmoLong = 123456789
+
+    val int = 123
+    val long: Long = int.toLong()
+
+}
